338366e26a0cc3f6cdcd3ab1f818a7013dd6293c
ailene808 <54044885+ailene808@users.noreply.github.com>
2024-02-02 06:33:25 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String phrase = "This class is fun but hard at the same time. I like the professor's music in his videos.";
+        String result = "";
+        for(int i = phrase.length()-1; i>=0; i--)
+            result+=phrase.charAt(i);
+        System.out.println(result);
+
+    }
+}
