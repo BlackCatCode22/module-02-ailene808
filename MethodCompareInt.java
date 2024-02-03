9975a66b5cf1c3f618269539fc991515255f8661
ailene808 <54044885+ailene808@users.noreply.github.com>
2024-02-02 20:01:06 -0800

    Add files via upload

---
+++ MethodCompareInt.java
@@ -0,0 +1,16 @@
+public class MethodCompareInt {
+    public static void main(String[] args)
+
+    {
+                int a = 5;  //Establishes a value of 5 for a
+                int b = 10; //Establishes a value of 10 for b
+
+                System.out.println(Integer.compare(a, b)); // The compare() method compare 2 integer values and returns a value of zero if (a==b) if (a<b) value will be less than zero
+
+                int c = 5;
+                int d = 5;
+
+                System.out.println(Integer.compare(c, d));
+
+                  }
+}
